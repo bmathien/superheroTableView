ed68cd8cde3d78d3fa81e2588aa2b9a525b64e06
Mathien <mathien@HS-DU-C138-26.local>
2016-01-15 09:38:17 -0600

    custom class and images

---
+++ tableView/SuperHero.swift
@@ -0,0 +1,33 @@
+//
+//  SuperHero.swift
+//  tableView
+//
+//  Created by Mathien on 1/13/16.
+//  Copyright © 2016 Mathien. All rights reserved.
+//
+
+import UIKit
+
+class SuperHero: NSObject
+{
+    var name = ""
+    var alias = ""
+    var power = 0
+    var image = UIImage(named: "Default")
+    
+
+    init(Name: String, Alias: String, Power: Int, Image: UIImage)
+    {   super.init()
+        name = Name
+        alias = Alias
+        power = Power
+        image = Image
+    }
+    
+    init(Name: String, Alias: String)
+    {
+        super.init()
+        name = Name
+        alias = Alias
+    }
+}
